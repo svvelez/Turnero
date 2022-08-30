1851beeb26f4e477f7a71f3d88e995c40038a26f
Aprendiz TIC <Aprendiz TIC@TIC011.dime.net.co>
2022-08-30 14:16:42 -0500

    ingresar documento

--- resources/views/ingresardocumento.blade.php
+++ resources/views/ingresardocumento.blade.php
@@ -8,6 +8,10 @@
     <link href="{{ asset('assets/css/@fortawesome/fontawesome-free/css/all.css') }}" rel="stylesheet" type="text/css">
     <script src="https://kit.fontawesome.com/6fe6c76279.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+
+{{--     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"> --}}
 
 
 </head>
@@ -24,6 +28,10 @@
     <a class="siguiente" href="#">Siguiente <i class="fas fa-arrow-right"></i> </a>
 </div>
 
+<div class="contenido">
+    <h1>Ingrese su número de documento</h1>
+    <input type="text" name="numerodocumento">
+</div>
 
 
 
